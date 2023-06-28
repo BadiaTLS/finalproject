ffbf274a3e2d37daaeec03e543c3f32ca9d94a2f
esutrisno94 <esutrisno94@students.calvin.ac.id>
2023-06-29 06:34:24 +0700

    Cleaning up

--- final_project/final_project/sas/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.sas_index, name='sas_index'),
-    path('import_user', views.import_user, name='import_user'),
-    path('import_class', views.import_class, name='import_class'),
-    path('not_sas', views.not_sas, name='not_sas'),
-]
