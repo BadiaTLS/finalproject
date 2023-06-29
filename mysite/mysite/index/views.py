7fa675e53db5aa67e1340c9151ce0f6063c28788
esutrisno94 <esutrisno94@students.calvin.ac.id>
2023-06-29 08:01:46 +0700

    Cleaning up

--- mysite/mysite/index/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-import os
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, There, You did it. It worked out")
