ba3dfcd3fba5a4490e7263564bec4605a7935833
esutrisno94 <esutrisno94@students.calvin.ac.id>
2023-07-11 20:50:12 +0700

    Changes to bat files

--- run.bat
+++ run.bat
@@ -1,10 +1,10 @@
+@echo off
 call %~dp0tavenv\Scripts\activate.bat
 set PROMPT=(tavenv) $P$G
 
 echo %VIRTUAL_ENV%
 echo %PROMPT%
 
-python final_project\manage.py runserver
+python final_project\manage.py runserver -v 3
 
 call tavenv\Scripts\deactivate.bat
-pause
--- setup.bat
+++ setup.bat
@@ -1,33 +1,21 @@
+@echo off
+echo Starting Setup.bat
 if exist %~dp0tavenv rmdir /s /q %~dp0tavenv (
+    echo Creating Environment called tavenv
     python -m venv tavenv
     echo tavenv created
     echo installing requirements.txt
-    timeout /t 5
+    timeout /t 3
     call %~dp0tavenv\Scripts\activate.bat
     set PROMPT=(tavenv) $P$G
 
-    if not exist %~dp0final_project\requirements.txt (
-        echo final_project\requirements.txt not found
-        exit /b
-    )
-    pip install -r %~dp0final_project\requirements.txt
-    call %~dp0tavenv\Scripts\deactivate.bat
-) else (
-    echo installing requirements.txt
-    timeout /t 5
-    call %~dp0tavenv\Scripts\activate.bat
-    set PROMPT=(tavenv) $P$G
-
-    if not exist %~dp0final_project\requirements.txt (
-        echo %~dp0final_project\requirements.txt not found
-        exit /b
-    )
     pip install -r %~dp0final_project\requirements.txt
     call %~dp0tavenv\Scripts\deactivate.bat
 )
 
+cls
 echo Getting Ready for migrations
-timeout /t 5
+timeout /t 3
 
 call %~dp0tavenv\Scripts\activate.bat
 set PROMPT=(tavenv) $P$G
@@ -44,10 +32,11 @@ set DJANGO_SUPERUSER_PASSWORD=admin
 python final_project\manage.py createsuperuser --noinput
 
 @echo off
-echo from final_project.accounts.models import CustomUser; CustomUser.objects.create_user(username='sassas', email='sas@example.com', password='sassas', name='SAS', role='sas') | python final_project\manage.py shell
-echo from final_project.accounts.models import CustomUser; CustomUser.objects.create_user(username='dininghall', email='dininghall@example.com', password='dininghall', name='Dining Hall', role='dininghall') | python final_project\manage.py shell
+echo from final_project.accounts.models import CustomUser; CustomUser.objects.create_user(username='sassas', email='sas@example.com', password='sassas', name='SAS', role='sas', gender='-', major='-') | python final_project\manage.py shell
+echo from final_project.accounts.models import CustomUser; CustomUser.objects.create_user(username='dininghall', email='dininghall@example.com', password='dininghall', name='Dining Hall', role='dininghall', gender='-', major='-') | python final_project\manage.py shell
 
-python final_project\manage.py runserver
+cls
+python final_project\manage.py runserver -v 0
 
 call tavenv\Scripts\deactivate.bat
 
